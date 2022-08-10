50c5c1076cd066823cb92760724518631a8f22ff
Matheus Wesley <67769338+CheckMaathy@users.noreply.github.com>
2022-08-10 13:14:34 -0300

    feat: add endpoint with simple string interpolation

--- src/handler/mod.rs
+++ src/handler/mod.rs
@@ -1,6 +1,13 @@
 // Handlers
 
+use rocket::http::RawStr;
+
 #[get("/")]
 pub fn hello() -> &'static str {
     "Hello, world!"
+}
+
+#[get("/hello/<name>")]
+pub fn userByName(name: String) -> String {
+    format!("Hello, {name}")
 }
--- src/rust_application.rs
+++ src/rust_application.rs
@@ -1,8 +1,9 @@
 #[macro_use] extern crate rocket;
 
 use lib::handler::hello;
+use lib::handler::userByName;
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
+    rocket::build().mount("/", routes![hello, userByName])
 }
